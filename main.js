7f794d2d0b8f7633f1e5d17e48e2242a6b541bd6
PamiSamiK <144476939+PamiSamiK@users.noreply.github.com>
2024-01-17 13:13:29 -0500

    3eT
    1

---
+++ main.js
@@ -0,0 +1,19 @@
+import 
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+  }
+  
+  console.log(getRandomInt(500));
+  // Expected output: 0, 1 or 2
+  
+  console.log(getRandomInt(1));
+  // Expected output: 0
+  
+  console.log(Math.random());
+  // Expected output: a number from 0 to <1
+  
+
+[1, 2, 3, 4]
+3, 4 2, 1
+{"1":"red", }
+
